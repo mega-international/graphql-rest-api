2f4010785fa6a0d37047157d80485f37d1f9ff91
Olivier GUIMARD <57092779+oguimardmega@users.noreply.github.com>
2022-06-29 15:07:40 +0200

    V5 CP3 update

--- GeneratorMappingJSON/V5/05 - EXE/run.bat
+++ GeneratorMappingJSON/V5/05 - EXE/run.bat
@@ -1,1 +1,1 @@
-"C:\ProgramData\MEGA\Hopex Application Server\5000\.shadowFiles\hopex.core\15.0.0+5947\java\jre\bin\java.exe" -jar "C:\temp\java\generatorJSONMapping.jar" -d -f "c:\temp\java\\" -e "C:\Users\Public\Documents\HOPEX V4\PRESALESV4" -r SOHO -l "c:\temp\java\\"
+"C:\MEGA_HAS\HOPEX Application Server\5027\.shadowFiles\hopex.core\15.2.7+6181\java\jre\bin\java.exe" -jar "C:\temp\java\generatorJSONMapping.jar" -d -f "c:\temp\java\\" -e "C:\MEGA_HAS\HOPEX Application Server\Databases\5027\Environment\V5-CP2-HF7-Official_backup.envtestlab_15_2_0+6110" -r SOHO -l "c:\temp\java\\"
